ba1077aa05937372ef86a109531985cc9c7553ac
Katarzyna Niemiec <katarzyna.niemiec.programista@gmail.com>
2021-11-29 23:30:39 +0100

    Project configuration

---
+++ src/app/school-register-dashboard/school-register-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-school-register-dashboard',
+	templateUrl: './school-register-dashboard.component.html',
+	styleUrls: ['./school-register-dashboard.component.scss'],
+})
+export class SchoolRegisterDashboardComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
